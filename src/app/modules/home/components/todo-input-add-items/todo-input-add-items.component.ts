b776917e81ede43cd41049bdb629e2d0a202c288
Lucas Felipe da Silva Pires <76564585+LFPires1993@users.noreply.github.com>
2023-06-18 22:39:53 -0300

    change css to scss

--- src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
+++ src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todo-input-add-items',
   templateUrl: './todo-input-add-items.component.html',
-  styleUrls: ['./todo-input-add-items.component.css']
+  styleUrls: ['./todo-input-add-items.component.scss']
 })
 export class TodoInputAddItemsComponent {
 
